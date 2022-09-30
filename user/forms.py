1f67c16e7ca09a8b1363ef09e201d81f124f1ee2
AKINBOBOLA <emmanuelakins908@gmail.com>
2022-09-30 23:43:41 +0100

    added search.html and made changes to some other templates

--- user/forms.py
+++ user/forms.py
@@ -3,6 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+
+
+
+
+
+
     # def __init__(self, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
     #     self.fields["username"].widget.attrs.update({
@@ -43,8 +54,3 @@ class SignUpForm(UserCreationForm):
     #         "maxlength": "22",
     #         "minlength": "8",
     #     })
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
-
